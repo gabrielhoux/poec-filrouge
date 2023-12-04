60b8f7c9efc56e1a9bbfb4d49996fb8d270effa4
gabrielhoux <gabriel.houx@gmail.com>
2023-12-04 12:19:51 +0100

    +paramètre  pour méthodes du contrôleur Ingredient et ses routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IngredientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Routes d'accès aux ingrédients
+Route::get('/ingredients', [IngredientController::class, 'index']);
+Route::post('/ingredients', [IngredientController::class, 'store']);
+Route::get('/ingredients/{id}', [IngredientController::class, 'show']);
+Route::delete('/ingredients/{id}', [IngredientController::class, 'destroy']);
