25160d850990f720617d23b102129213538024e0
messaoudi nora <messanora86@gmail.com>
2023-12-05 14:52:08 +0100

    ajoute different critére s de formulaire

--- resources/views/fromIngredient.blade.php
+++ resources/views/fromIngredient.blade.php
@@ -3,46 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ingredient</title>
+    <title>Ingrédient</title>
+</head>
 
 <body>
-<div class="container">
+    <div class="container">
+        <h1>Nouvel ingrédient</h1>
+        <form method="post">
+            @csrf
+            <input id="name" name="name" type="text" placeholder="Ajouter votre ingrédient" required/>
+            <button type="submit">Ajouter</button>
+        </form>
+    </div>
 
-<h1>Nouvel ingrédient</h1>
-<for method="post">
- @csrf
-    <label for="name">Nom</label>
-    <input id ="name" name ="name" type="text" required/>
-    <button type="submit">Enregister</button>
-</for>
+    <div class="container">
+        <select class="form-select" aria-label="Régime">
+            <option selected>Régime</option>
+            <option value="végé">végé</option>
+            <option value="vegan">vegan</option>
+            <option value="omni">Omni</option>
+            <option value="pesca">pesca</option>
+        </select>
+    </div>
+    <br>
 
-</div>
+    <div class="container">
+        <select class="form-select" aria-label="Catégories">
+            <option selected>Catégories</option>
+            <option value="Apéritif">Apéritifs</option>
+            <option value="entrée">Entrées</option>
+            <option value="plat">Plats</option>
+            <option value="Dessert">Desserts</option>
+            <option value="BOISSON">Boissons</option>
+        </select>
+    </div>
+    <br>
 
-<div class="container">
-<select class="form-select" aria-label="Default select example">
-<option selected>Régime</option>
-<option value="végé">végé</option>
-<option value="vegan">vegan</option>
-<option value="omni">Omni</option>
-<option value="pesca">pesca</option>
-</select>
-</div>
+    <div class="container">
+        <div><span>Nombre de portions </span></div>
+        <button onclick="decrement()">-</button>
+        <span id="counter">0</span>
+        <button onclick="increment()">+</button>
+    </div>
 
-<div class="container">
-<select class="form-select" aria-label="Default select example">
-<option selected>Categories </option>
-<option value="Apéritif">Apéritifs</option>
-<option value="entrée">entrée</option>
-<option value="plat">plat</option>
-<option value="Dessert">Dessert</option>
-<option value="BOISSON">BOISSON </option>
+    <form action="traitement.php" method="post">
+        <div class="container">
+            <label for="tempsPreparation">Temps de préparation :</label>
+            <input type="number" id="tempsPreparation" name="tempsPreparation" placeholder="Entrez le temps de préparation" min="0" step="5" required>
+            <span> minutes</span>
+        </div>
 
+        <div class="container">
+            <input type="checkbox" id="moinsCalorique" name="moinsCalorique">
+            <label for="moinsCalorique">Moins calorique</label>
+        </div>
 
-
-
-</select>
-</div>
-    
+        <input type="submit" value="Envoyer">
+    </form>
 </body>
 </html>
-
