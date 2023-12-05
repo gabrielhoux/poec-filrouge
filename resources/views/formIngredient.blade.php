a0ef3eecc238bb0f5f30cacf54668782bbb21ba7
gabrielhoux <gabriel.houx@gmail.com>
2023-12-05 15:42:04 +0100

    nouveau form ingredients

--- resources/views/formIngredient.blade.php
+++ resources/views/formIngredient.blade.php
@@ -1,31 +1,53 @@
-        <div class="container">
-            <h1>Nouvel ingrédient</h1>
-            <form method="post">
-            @csrf
-                <label for="name">Nom</label>
-                <input id ="name" name ="name" type="text" required/>
-                <button type="submit">Enregister</button>
-            </form>
-        </div>
+<div class="container">
+    <h1>Nouvel ingrédient</h1>
+    <form method="post">
+        @csrf
+        <input id="name" name="name" type="text" placeholder="Ajouter votre ingrédient" required/>
+        <button type="submit">Ajouter</button>
+    </form>
+</div>
 
-        <div class="container">
-            <select class="form-select" aria-label="select-regime">
-                <option selected>Régime</option>
-                <option value="végé">Végétarien</option>
-                <option value="vegan">Vegan</option>
-                <option value="omni">Omnivore</option>
-                <option value="pesca">Pescatarien</option>
-            </select>
-        </div>
+<div class="container">
+    <select class="form-select" aria-label="Régime">
+        <option selected>Régime</option>
+        <option value="végé">végé</option>
+        <option value="vegan">vegan</option>
+        <option value="omni">Omni</option>
+        <option value="pesca">pesca</option>
+    </select>
+</div>
+<br>
 
-        <div class="container">
-            <select class="form-select" aria-label="select-categorie">
-                <option selected>Catégorie</option>
-                <option value="apéritif">Apéritif</option>
-                <option value="entrée">Entrée</option>
-                <option value="plat">Plat</option>
-                <option value="dessert">Dessert</option>
-                <option value="boisson">Boisson</option>
-            </select>
-        </div>
+<div class="container">
+    <select class="form-select" aria-label="Catégories">
+        <option selected>Catégories</option>
+        <option value="Apéritif">Apéritifs</option>
+        <option value="entrée">Entrées</option>
+        <option value="plat">Plats</option>
+        <option value="Dessert">Desserts</option>
+        <option value="BOISSON">Boissons</option>
+    </select>
+</div>
+<br>
 
+<div class="container">
+    <div><span>Nombre de portions </span></div>
+    <button onclick="decrement()">-</button>
+    <span id="counter">0</span>
+    <button onclick="increment()">+</button>
+</div>
+
+<form action="traitement.php" method="post">
+    <div class="container">
+        <label for="tempsPreparation">Temps de préparation :</label>
+        <input type="number" id="tempsPreparation" name="tempsPreparation" placeholder="Entrez le temps de préparation" min="0" step="5" required>
+        <span> minutes</span>
+    </div>
+
+    <div class="container">
+        <input type="checkbox" id="moinsCalorique" name="moinsCalorique">
+        <label for="moinsCalorique">Moins calorique</label>
+    </div>
+
+    <input type="submit" value="Envoyer">
+</form>
