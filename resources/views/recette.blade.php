d0476332d2f5520aa79c93434001cd95b9f5051d
gabrielhoux <gabriel.houx@gmail.com>
2023-12-07 13:39:44 +0100

    sauvegarde

--- resources/views/recette.blade.php
+++ resources/views/recette.blade.php
@@ -1,22 +1,46 @@
-<div>
-    <h1>{{ $donnees['titre'] }}</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/main.js'])
+</head>
+<body>
 
-    <!-- Affichez les autres données ici -->
-    <div>
-       <h2>Liste des ingredients</h2>
-    <ul>
+    @isset($donnees)
+    
+    <div class="container is-fluid">
+        <div class="columns">
+            <div class="column is-half">
 
-        @foreach($donnees['ingredients'] as $ingredient)
-            <li>{{ $ingredient }}</li>
-        @endforeach
-    </ul>
-   </div>
-    <div>
-    <ol>
-        @foreach($donnees['instructions'] as $instruction)
-        <li>{{ $instruction }}</li>
-        @endforeach
-    </ol>
+                <h1>{{ $donnees['titre'] }}</h1>
 
+                <!-- Affichez les autres données ici -->
+                <div>
+                <h2>Liste des ingredients</h2>
+                    <ul>
+                        @foreach($donnees['ingredients'] as $ingredient)
+                        <li>{{ $ingredient }}</li>
+                        @endforeach
+                    </ul>
+            </div>
+            
+                <div>
+                    <ol>
+                        @foreach($donnees['instructions'] as $instruction)
+                        <li>{{ $instruction }}</li>
+                        @endforeach
+                    </ol>
+                </div>
+            </div>
+            <div class="column is-half">
+                <img alt="Crêpes sucrées" class="imgRecipe" fetchpriority="high" height="660" src="https://img.cuisineaz.com/660x660/2015/05/26/i98246-crepes-sucrees.jpg" width="660">
+            </div>
     </div>
-</div>
+
+    @endisset
+    
+</body>
+</html>
