ebe2d34b0c3162abc1e4727e15d6ccd4b48fe26d
gabrielhoux <gabriel.houx@gmail.com>
2023-12-10 16:46:13 +0100

    script cookies amélioré pour que bannière affiche seulement première visite

--- resources/js/cookies.js
+++ resources/js/cookies.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cookiesBtns = document.querySelectorAll('.cookies-btn');
+    const acceptCookiesBtn = document.getElementById('acceptCookies');
+    const refuseCookiesBtn = document.getElementById('refuseCookies');
     const cookiesBanner = document.getElementById('cookiesBanner');
 
-    cookiesBanner.style.display = "block";
+    // Check if the user has already made a choice about cookies
+    const cookiesChoiceMade = localStorage.getItem('cookiesChoiceMade');
+    console.log('cookiesChoiceMade:', cookiesChoiceMade); // Debugging line
 
-    cookiesBtns.forEach(btn => {
-        btn.addEventListener('click', (event) => {
-            cookiesBanner.style.display = "none";
-        });
+    if (!cookiesChoiceMade) {
+        cookiesBanner.style.display = "block";
+    } else {
+        cookiesBanner.style.display = "none";
+    }
+
+    // If the user accepts cookies
+    acceptCookiesBtn.addEventListener('click', () => {
+        // Store the user's choice in localStorage
+        localStorage.setItem('cookiesChoiceMade', 'accepted');
+        cookiesBanner.style.display = "none";
+    });
+
+    // If the user refuses cookies
+    refuseCookiesBtn.addEventListener('click', () => {
+        // Store the user's choice in localStorage
+        localStorage.setItem('cookiesChoiceMade', 'refused');
+        cookiesBanner.style.display = "none";
     });
 });
--- resources/views/cookiesFooter.blade.php
+++ resources/views/cookiesFooter.blade.php
@@ -5,8 +5,8 @@
 
         <p>Ce site utilise des <b>cookies</b>. Acceptez-vous leur utilisation ?</p>
 
-        <button class="cookies-btn button is-primary">Accepter</button>
-        <button class="cookies-btn button is-light">Refuser</button>
+        <button id="acceptCookies" class="cookies-btn button is-primary">Accepter</button>
+        <button id="refuseCookies" class="cookies-btn button is-light">Refuser</button>
 
     </div>
 </div>
