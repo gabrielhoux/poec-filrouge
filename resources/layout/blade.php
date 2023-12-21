297082b03634684fff1163a5341169e5c91dde13
jose <jjoseceppic@gamail.cm>
2023-12-21 09:59:35 +0100

    sauvegarde

---
+++ resources/layout/blade.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+            @include('cookiesFooter')
+
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                </p>
+            </div>
+        </footer>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>À propos de nous</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam ultricies, urna nunc ultrices nunc, id lacinia velit nunc id nunc.</p>
+                </div>
+                <div class="col-md-6">
+                    <h3>Contactez-nous</h3>
+                    <ul>
+                        <li><i class="fa fa-phone"></i> +33 123 456 789</li>
+                        <li><i class="fa fa-envelope"></i> info@example.com</li>
+                        <li><i class="fa fa-map-marker"></i> 123 Rue du Commerce, Paris</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        *
+        <div class="content has-text-centered">
+            <p>
+                <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,17 +9,21 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     @vite(['resources/css/app.css', 'resources/js/main.js'])
 </head>
+
 <body>
     <section class="section is-small">
         <h1 class="title">M•IA•M</h1>
         <h2 class="subtitle">
-          Besoin d'une <strong>recette</strong> ?
+            Besoin d'une <strong>recette</strong> ?
         </h2>
-
         @include('formIngredient')
+        </h1>
     </section>
-    <footer class="footer">
+
+    <footer>
         @include('cookiesFooter')
+        <p>Copyright du site web de test <a href="#">Lien externe</a></p>
     </footer>
 </body>
+
 </html>
