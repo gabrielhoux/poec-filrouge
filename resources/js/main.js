21cedc3e1f5d8c4013cab37f9e1420ffa1aa9190
gabrielhoux <gabriel.houx@gmail.com>
2024-01-03 16:24:32 +0100

    modif formulaire cookies pour acceptation = condition pour matomo

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,19 +3,23 @@ import './cookies.js';
 import './fetchAPI.js';
 import $ from 'jquery';
 
-// Matomo script
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function()
+
+if (cookiesAccepted == true)
 {
-    var u="//localhost/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
+    // Matomo script
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function()
+    {
+        var u="//localhost/matomo/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+}
 
 // Affichage du formulaire et dissimulation de la recette par défaut
 $(function()
