ce8bfbfb57102e480306229f66408a7e2a038de9
gabrielhoux <gabriel.houx@gmail.com>
2024-01-03 14:23:55 +0100

    scripts matomo insérés dans js

--- resources/js/formIngredient.js
+++ resources/js/formIngredient.js
@@ -1,21 +1,33 @@
 import { openModal, closeModal } from './loadingModal.js';
 import $ from 'jquery';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () =>
+{
     const ingredientInput = document.getElementById('ingredient-input');
     const addIngredientBtn = document.getElementById('add-ingredient');
     const ingredientList = document.getElementById('ingredient-list');
     const ingredientButtons = document.querySelectorAll('#ingredient-button');
 
-    addIngredientBtn.addEventListener('click', () => {
+    addIngredientBtn.addEventListener('click', () =>
+    {
         $('#input-message').text("");
         $('#ingredient-input').removeClass("is-danger");
         $('#input-icon-container').empty();
+
         const newIngredient = ingredientInput.value.trim();
-        if (/^[A-Za-z\s]+$/.test(newIngredient)) {
-            handleIngredientClick(newIngredient.toLowerCase());
+        if (/^[A-Za-z\s]+$/.test(newIngredient))
+        {
+            // Formattage minuscule de l'ingrédient
+            const formattedIngredient = newIngredient.toLowerCase();
+            // Ajout de l'ingrédient
+            handleIngredientClick(formattedIngredient);
+            // Ajout de l'évenement à Matomo
+            _paq.push(['trackEvent', 'Button click', 'Ajout ingrédient manuel', formattedIngredient]);
+            // Réinitialisation du champs
             ingredientInput.value = '';
-        } else {
+        }
+        else
+        {
             $('#input-message').text("Entrée invalide");
             $('#ingredient-input').addClass("is-danger");
             $('#input-icon-container').append("<i class='fas fa-exclamation-triangle'></i>");
@@ -24,16 +36,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Ajout d'ingrédient via bouton "Ajouter" avec handleIngredientClick
-    ingredientButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
+    ingredientButtons.forEach(button =>
+    {
+        button.addEventListener('click', (event) =>
+        {
             const ingredient = event.target.textContent;
+            // Ajout de l'ingrédient
             handleIngredientClick(ingredient);
+            // Ajout de l'évenement à Matomo
+            _paq.push(['trackEvent', 'Button click', 'Ajout ingrédient', ingredient]);
         });
     });
 
     // Supprimer un ingrédient en cliquant dessus dans la liste
-    ingredientList.addEventListener('click', (event) => {
-        if (event.target.tagName === 'i') {
+    ingredientList.addEventListener('click', (event) =>
+    {
+        if (event.target.tagName === 'i')
+        {
             const li = event.target.parentElement;
             li.remove();
         }
@@ -41,7 +60,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Gérer la soumission du formulaire
     const form = document.getElementById('ingredient-form');
-    form.addEventListener('submit', async (event) => {
+    form.addEventListener('submit', async (event) =>
+    {
         event.preventDefault(); // Empêche la soumission par défaut
         const ingredients = [...ingredientList.querySelectorAll('#ingredient-element')].map(el => el.textContent);
         console.log(ingredients); // Affiche les ingrédients à soumettre (remplacez par l'envoi AJAX ou la manipulation des données)
@@ -52,12 +72,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const tempsPreparation = document.getElementById('temps-preparation').value;
         const legerCheckbox = document.getElementById('legerCheckbox');
 
-        console.log('Regime sélectionné :', selectedRegime);
-        console.log('type sélectionné :', selectedType);
-        console.log('nombre de portion :', portionnbre);
-        console.log('temps de Preparation est :', tempsPreparation ,'minute');
-        console.log('legerCheckbox :', legerCheckbox.checked);
-
         const data = { "ingredients": ingredients }; // Inclure toujours les ingrédients
         // Vérification des valeurs optionnelles
         if (selectedRegime !== "") data.selectedRegime = selectedRegime;
@@ -67,6 +81,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (legerCheckbox.checked) data.legerCheckbox = true;
 
         console.log(data);
+
+        // Push custom event to track form submission with data submitted
+        _paq.push(['trackEvent', 'Form', 'Submission', `${data}`]);
 
         try {
             // Envoi des données à la fonction main
@@ -83,9 +100,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     const cancelButton = document.getElementById('cancelButton');
-    cancelButton.addEventListener('click', () => {
+    cancelButton.addEventListener('click', () =>
+    {
         // Réinitialiser le formulaire
         form.reset();
+
+        // Ajout de l'évenement à Matomo
+        _paq.push(['trackEvent', 'Button click', 'Réinitialisation du formulaire']);
     
          // Réinitialiser la liste des ingrédients
         ingredientList.innerHTML  = '';
@@ -104,7 +125,9 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Ajout d'ingrédient à la liste, sous forme d'un bouton avec une icône X pour l'effacer si on clique dessus
-function handleIngredientClick(ingredient) {
+function handleIngredientClick(ingredient)
+{
+    // Initialisation de l'élément li sous forme d'un bouton
     const $li = $(`
         <li class="button is-outlined is-primary">
             <span id="ingredient-element">${ingredient}</span>
@@ -114,8 +137,10 @@ function handleIngredientClick(ingredient) {
         </li>
     `);
 
+    // Ajout de l'élément li à la liste d'ingrédient
     $('#ingredient-list').append($li);
 
+    // Suppression de l'élément de la liste si icône X cliquée
     $li.find('.remove-ingredient').on('click', () => {
         $li.remove();
     });
--- resources/js/main.js
+++ resources/js/main.js
@@ -3,12 +3,32 @@ import './cookies.js';
 import './fetchAPI.js';
 import $ from 'jquery';
 
-$(function() {
+// Matomo script for analytics
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+var u="//localhost/matomo/";
+_paq.push(['setTrackerUrl', u+'matomo.php']);
+_paq.push(['setSiteId', '1']);
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+
+// Affichage du formulaire et dissimulation de la recette par défaut
+$(function()
+{
     $('#formulaire').show();
     $('#recette').hide();
 });
 
-$('#return').on('click', () => {
+// Affichage du formulaire et dissimulation de la recette en cliquant sur le bouton flèche #return
+$('#return').on('click', () =>
+{
     $('#formulaire').show();
     $('#recette').hide();
+
+    // Envoi de l'évenement à Matomo
+    _paq.push(['trackEvent', 'Button click', 'Return to form']);
 });
