c9b588cb7be7bc4d1bb161e61a5448f5a007dbb6
gabrielhoux <gabriel.houx@gmail.com>
2024-01-04 13:50:29 +0100

    tests footer avec logos openai et ib cegos

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -35,6 +35,36 @@
     @include('loadingModal')
 
     <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                Cuisiné avec goût par Gabriel Houx, Nora Messaouidi, Maria de Carvalho & José
+            </p>
+            <p>
+                Tous droits réservés • <strong>mIAm</strong> 2023-2024
+            </p>
+        </div>
+        <div class="columns is-mobile mt-4">
+            <div class="column is-one-quarter"></div>
+            <div class="column is-two-quarter">
+                <div class="columns is-mobile">
+                    <div class="column logo-container is-vcentered">
+                        <figure class="image is-48x48">
+                            <a href="https://openai.com/">
+                                <img src="https://freelogopng.com/images/all_img/1681142382OpenAI-png.png" alt="openai-logo">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column logo-container is-vcentered">
+                        <figure class="image is-48x48">
+                            <a href="https://www.ib-formation.fr/">
+                                <img src="https://www.ib-formation.fr/vendor/laravel-theme-inter/dirAssetse694b50/media/img/logo-ib.png" alt="ib-cegos-logo">
+                            </a>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-one-quarter"></div>
+        </div>
         @include('cookiesFooter')
     </footer>
 </body>
